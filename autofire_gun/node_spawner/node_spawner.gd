a09071622b98847964c376e0bc1cd0ff4547bf72
tab <tab@noreply.codeberg.org>
2023-12-16 17:28:58 +0000

    feat: remove enemy on hit

--- autofire_gun/node_spawner/node_spawner.gd
+++ autofire_gun/node_spawner/node_spawner.gd
@@ -5,14 +5,21 @@ extends Node2D
 
 
 @export var nodeScene:PackedScene = null
-
+var nodeContainer = self
 
 func _ready():
 	assert(nodeScene, name + ": you need to set the nodeScene to spawn")
 	
+	var level_bullet_container:Node = null
+	level_bullet_container = get_tree().get_nodes_in_group("bullets_container")[0]
+	if level_bullet_container:
+		nodeContainer = level_bullet_container
+	else:
+		push_warning("nodeContainer should not be itself. Otherwise deleting the spawner will also delete the bullets")
+		
 
 func _spawn_node():
 	var node:Node = nodeScene.instantiate()
-	add_child(node)
+	nodeContainer.add_child(node)
 	node.position = global_position
 	node.top_level = true
--- enemy/enemy_bullet_hitbox.gd
+++ enemy/enemy_bullet_hitbox.gd
@@ -6,5 +6,4 @@ extends Area2D
 
 func _on_area_entered(area: Area2D) -> void:
 	print("enemy hit")
-	
-
+	get_parent().queue_free()
