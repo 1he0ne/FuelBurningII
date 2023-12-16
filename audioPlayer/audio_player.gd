7058608ade812a6add6abcff224de054e8c31ea2
Eduard Pranz <1703839+1he0ne@users.noreply.github.com>
2023-12-16 23:31:38 +0100

    added multiple audio channels for SFX

--- audioPlayer/audio_player.gd
+++ audioPlayer/audio_player.gd
@@ -1,13 +1,25 @@
 extends Node
 
+var sfx_channels
+var channel_id_to_play_on = 0
+
+func cycle_channel():
+	channel_id_to_play_on = (channel_id_to_play_on+1) % sfx_channels.size()
+
+func get_next_channel() -> AudioStreamPlayer:
+	cycle_channel()
+	print("current channel: %s" % channel_id_to_play_on)
+	return sfx_channels[channel_id_to_play_on]
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("audio player loaded")
-	pass # Replace with function body.
+	sfx_channels = [$SFX1, $SFX2, $SFX3, $SFX4, $SFX5, $SFX6, $SFX7, $SFX8]
 
 func play_sfx(wav_to_play: AudioStreamWAV):
-	$SFX.stream = wav_to_play
-	$SFX.play()
+	var channel: AudioStreamPlayer = get_next_channel()
+	channel.stream = wav_to_play
+	channel.play()
 	
 func play_bgm(wav_to_play: AudioStreamWAV):
 	$BGM.stream = wav_to_play
