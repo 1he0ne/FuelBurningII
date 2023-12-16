9cc20c1075a79d60d6b157f3a766ced17d5ae2a7
tab <tab@noreply.codeberg.org>
2023-12-16 16:19:54 +0000

    feat: add collisions between bullets and player/enemy

---
+++ character/character_bullet_hitbox.gd
@@ -0,0 +1,8 @@
+extends Area2D
+## Player area to take damage
+##
+## 
+
+
+func _on_area_entered(area: Area2D) -> void:
+	print("player hit")
---
+++ enemy/enemy_bullet_hitbox.gd
@@ -0,0 +1,10 @@
+extends Area2D
+## enemy area to take damage
+##
+## 
+
+
+func _on_area_entered(area: Area2D) -> void:
+	print("enemy hit")
+	
+
