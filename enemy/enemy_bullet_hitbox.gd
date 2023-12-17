ab3c8ab148383eefc2a64d0a9d3928da1b7fd869
tab <tab@noreply.codeberg.org>
2023-12-17 11:37:16 +0000

    feat: add enemy flash red on hit

--- enemy/enemy_bullet_hitbox.gd
+++ enemy/enemy_bullet_hitbox.gd
@@ -6,8 +6,11 @@ extends Area2D
 
 @export var fadeout_time = 0.4
 @export var max_health = 5.0
+@export var hit_flash_time = 0.05
+
 var health = max_health
 var is_alive := true
+
 
 var hit_cue: AudioStreamWAV
 var explode_cue: AudioStreamWAV
@@ -36,23 +39,37 @@ func _on_area_entered(area: Area2D) -> void:
 	var bullet_damage: float = bullet_that_hit.damage
 	bullet_that_hit.queue_free()
 	
-	health = clampf(health - bullet_damage, 0.0, max_health) 
+	health = clampf(health - bullet_damage, 0.0, max_health)
+	
+	damage_flash_animation(get_parent())
 	
 	if health <= 0: 
 		is_alive = false 
 		fade_out_and_delete(get_parent())
 		
+		#prevent more firing
 		%Guns.queue_free() 
 		
 		%ExplosionAnimation.position = global_position
 		%ExplosionAnimation.visible = true
 		%ExplosionAnimation.play()
+		
 		AudioPlayer.play_sfx(explode_cue)
 	else:
 		AudioPlayer.play_sfx(hit_cue)
 
-func fade_out_and_delete(node):
+
+func fade_out_and_delete(enemy:Node2D):
 	var tween = create_tween()
 	tween.set_process_mode(Tween.TWEEN_PROCESS_IDLE)
-	tween.tween_property(node, "modulate" , Color(1, 1, 1, 0), fadeout_time)
-	tween.tween_callback(node.queue_free)
+	tween.tween_property(enemy, "modulate" , Color(1, 1, 1, 0), fadeout_time)
+	tween.tween_callback(enemy.queue_free)
+	
+
+func damage_flash_animation(enemy:Node2D):
+	var original_modulate = modulate
+	
+	var tween = create_tween()
+	tween.set_process_mode(Tween.TWEEN_PROCESS_IDLE)
+	tween.tween_property(enemy, "modulate" , Color.RED, hit_flash_time)
+	tween.tween_property(enemy, "modulate" , original_modulate, hit_flash_time)
