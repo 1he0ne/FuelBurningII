333e727344aacb78601f60be200b615f20c9ac8b
Eduard Pranz <1703839+1he0ne@users.noreply.github.com>
2023-12-17 16:39:22 +0100

    improved options menu

--- menu/options_menu.gd
+++ menu/options_menu.gd
@@ -3,10 +3,19 @@ extends Node2D
 const game_scene_path = "res://level/level.tscn"
 const main_menu_scene_path = "res://menu/main_menu.tscn"
 
+var sfx_sample_sound = preload("res://audioAssets/explode1.wav")
+
+@onready var sfxBar = $VBoxContainer/sfx_volume_bar as ProgressBar
+@onready var bgmBar = $VBoxContainer/bgm_volume_bar as ProgressBar
+@onready var selectedBar = sfxBar
 
 
 func _ready():
-	($sfx_volume_bar as ProgressBar).value = AudioPlayer.sfx_volume
+	sfxBar.value = AudioPlayer.sfx_volume
+	bgmBar.value = AudioPlayer.bgm_volume
+	
+	_on_select_bar(sfxBar)
+	
 
 # Called when the node enters the scene tree for the first time.
 func _input(event: InputEvent) -> void:
@@ -15,11 +24,55 @@ func _input(event: InputEvent) -> void:
 		# how do we decide if came here from the game or from the menu?
 		
 	if event.is_action_pressed("move_left"):
-		AudioPlayer.sfx_volume = clampf(AudioPlayer.sfx_volume - 5.0, 0.0, 100.0)
-		AudioPlayer.set_sfx_volume()
-		($sfx_volume_bar as ProgressBar).value = AudioPlayer.sfx_volume
+		if selectedBar == sfxBar:
+			_on_sfx_minus()
+		if selectedBar == bgmBar:
+			_on_bgm_minus()
 		
 	if event.is_action_pressed("move_right"):
-		AudioPlayer.sfx_volume = clampf(AudioPlayer.sfx_volume + 5.0, 0.0, 100.0)
-		AudioPlayer.set_sfx_volume()
-		($sfx_volume_bar as ProgressBar).value = AudioPlayer.sfx_volume
+		if selectedBar == sfxBar:
+			_on_sfx_plus()
+		if selectedBar == bgmBar:
+			_on_bgm_plus()
+		
+	if event.is_action_pressed("move_up") || event.is_action_pressed("move_down"):
+		if selectedBar == sfxBar:
+			_on_select_bar(bgmBar)
+		else:
+			_on_select_bar(sfxBar)
+
+
+func _on_select_bar(bar_to_select: ProgressBar):
+	if bar_to_select == bgmBar:
+		sfxBar.modulate = Color.WHITE
+		selectedBar = bgmBar
+		bgmBar.modulate = Color.AQUA
+	else:
+		bgmBar.modulate = Color.WHITE
+		selectedBar = sfxBar
+		sfxBar.modulate = Color.AQUA
+	
+func _mod_sfx_volume(volume_to_add: float):
+	AudioPlayer.sfx_volume = clampf(AudioPlayer.sfx_volume + volume_to_add, 0.0, 100.0)
+	AudioPlayer.set_sfx_volume()
+	sfxBar.value = AudioPlayer.sfx_volume
+	AudioPlayer.play_sfx(sfx_sample_sound)
+	_on_select_bar(sfxBar)
+
+func _on_sfx_minus():
+	_mod_sfx_volume(-5.0)
+
+func _on_sfx_plus():
+	_mod_sfx_volume(+5.0)
+
+func _on_bgm_minus():
+	AudioPlayer.bgm_volume = clampf(AudioPlayer.bgm_volume - 5.0, 0.0, 100.0)
+	AudioPlayer.set_bgm_volume()
+	bgmBar.value = AudioPlayer.bgm_volume
+	_on_select_bar(bgmBar)
+
+func _on_bgm_plus():
+	AudioPlayer.bgm_volume = clampf(AudioPlayer.bgm_volume + 5.0, 0.0, 100.0)
+	AudioPlayer.set_bgm_volume()
+	bgmBar.value = AudioPlayer.bgm_volume
+	_on_select_bar(bgmBar)
