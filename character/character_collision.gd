59297b8f87156f8d85de37fc0ebdb96e167672ac
tab <tab@noreply.codeberg.org>
2023-12-17 11:50:56 +0000

    feat: add player damaged flash

--- character/character_collision.gd
+++ character/character_collision.gd
@@ -3,6 +3,8 @@ extends Area2D
 ##
 ## 
 
+@export var hit_flash_time = 0.1
+
 var collect_pickup_sfx: AudioStreamWAV = preload("res://audioAssets/upgradecollected1.wav")
 var lose_life_sfx: AudioStreamWAV = preload("res://audioAssets/explode1.wav")
 
@@ -20,6 +22,7 @@ func _on_area_entered(area: Area2D) -> void:
 		bomb_pickup.queue_free()
 	
 	print("player was hit by %s" % area.get_parent().name)
+	damage_flash_animation()
 
 
 func do_bullet_hit_stuff():
@@ -31,3 +34,10 @@ func do_bomb_pickup_stuff():
 	GameState.add_bombs(1)
 	AudioPlayer.play_sfx(collect_pickup_sfx)
 	print("Character gained a bomb (%s total)" % GameState.num_bombs)
+
+
+func damage_flash_animation():
+	var tween = create_tween()
+	tween.set_process_mode(Tween.TWEEN_PROCESS_IDLE)
+	tween.tween_property(get_parent(), "modulate", Color.LIGHT_SLATE_GRAY, hit_flash_time)
+	tween.tween_property(get_parent(), "modulate", Color(1,1,1,1), hit_flash_time)
