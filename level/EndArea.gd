e622fd3a3ec4b65d1f88bdbd290a8639dff2420d
tab <tab@noreply.codeberg.org>
2023-12-19 21:46:43 +0000

    feat: add warning banner, add more islands

--- level/EndArea.gd
+++ level/EndArea.gd
@@ -3,10 +3,28 @@ extends Area2D
 ##
 ##
 
+@export var warning_scene:PackedScene
+@export var time_paused := 3.0
+
+var done = false
+
 
 func _on_area_entered(_area: Area2D) -> void:
-	print("character has reached the end")
+	#print("character has triggered pausing")
+	if done:
+		queue_free()
+		return
+		
+	done = true
+
+	var new_scene = warning_scene.instantiate()
+	add_child(new_scene)
 	
-	#var character = area.get_parent()
-	#print("character.name: ", character.name)
+	_pause_for_a_time.call_deferred()
+
+
+func _pause_for_a_time():
+	get_tree().paused = true
 	
+	var unpause = func(): get_tree().paused = false
+	await get_tree().create_timer(time_paused).timeout.connect(unpause)
