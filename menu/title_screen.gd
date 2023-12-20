9ea525d46bdfc8d7c3e5b575f1b9dde8c0d21381
Eduard Pranz <1703839+1he0ne@users.noreply.github.com>
2023-12-20 01:20:17 +0100

    adjusted music volume

--- menu/title_screen.gd
+++ menu/title_screen.gd
@@ -7,9 +7,9 @@ var title_flash_counter = 0
 
 func _ready():
 	GameState.pause_timer()
-	AudioPlayer.play_sfx(AudioPlayer.fuel_burning_2_sfx, 0.8)
+	AudioPlayer.play_sfx(AudioPlayer.fuel_burning_2_sfx, 0.7)
 	await get_tree().create_timer(3.0).timeout
-	AudioPlayer.play_bgm(AudioPlayer.title_music) # enable, once title music has been added
+	AudioPlayer.play_bgm(AudioPlayer.title_music, 0.6) # enable, once title music has been added
 
 func _physics_process(delta):
 	title_flash_counter = title_flash_counter + 1
@@ -17,11 +17,11 @@ func _physics_process(delta):
 	
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("fire_weapon") || event.is_action_pressed("fire_bomb") || event.is_action_pressed("menu_back"): 
-		AudioPlayer.play_sfx(AudioPlayer.press_start_sfx)
+		AudioPlayer.play_sfx(AudioPlayer.press_start_sfx, 0.8)
 		AudioPlayer.stop_bgm()
 		await get_tree().create_timer(0.5).timeout
 		load_game_scene()
 		
 func load_game_scene():
-	AudioPlayer.play_bgm(AudioPlayer.intro_music)
+	AudioPlayer.play_bgm(AudioPlayer.intro_music, 1)
 	get_tree().change_scene_to_file(main_menu_path)
